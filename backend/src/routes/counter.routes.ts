8b41781a077ff497303110c28bd49c96e45a69ca
raphaelcatacutan <raphaelcatacutan@gmail.com>
2025-11-28 21:13:51 +0800

    Re-added queue management status

--- backend/src/routes/counter.routes.ts
+++ backend/src/routes/counter.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  activateCounter,
   closeCounter,
   getAllCounters,
 } from "../controllers/counter.controller.js";
@@ -8,13 +9,13 @@ const router = Router();
 
 router.get("/all", getAllCounters);
 router.post("/close", closeCounter);
+router.post("/activate", activateCounter);
 
 // TBR
 // router.get("/info", getCounterInfo);
 // router.get("/info/:sessionId", getCounterBySessionId);
 // router.get("/active", getActiveCounters);
 // router.get("/keys", getAvailableKeysHandler);
-// router.post("/activate", activateCounter);
 // router.post("/serve", serveApplicant);
 // router.post("/missing", markApplicantMissing);
 // router.post("/close-request", closeApplicantRequest);
